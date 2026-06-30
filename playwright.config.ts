8106a9a0a260d8ed3eb2ecaa5195a745288386c0
sowndariya <sowndariyadeveloper@gmail.com>
2026-06-30 16:34:59 +0530

    update html report

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-        ['html'],
+        ['html', { outputFolder: 'playwright-report', open: 'always' }],
         ['allure-playwright']
     ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
